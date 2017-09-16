b2092b4cafa0e58f56599c33a4b761648942dd50
sumanth80 <31929224+sumanth80@users.noreply.github.com>
2017-09-16 14:31:54 +0530

    Update StudentGroup.java

--- src/StudentGroup.java
+++ src/StudentGroup.java
@@ -132,37 +132,7 @@ public class StudentGroup implements StudentArrayOperation {
 
 	@Override
 	public void remove(Student student) {
-		Student x; boolean flag=false; int j;
-		if(student==null)
-			throw new IllegalArgumentException("Error");
-		else
-		{   
-			for(int i=0;i<students.length;i++)
-			{
-				if(students[i]==student)
-				{
-					flag=true;
-					j=i;
-					break;
-				}
-			}
-			if(flag==true)
-			{int index=j;
-				Student[] abc=new Student[students.length-1];
-				for(int i=0;i<index;i++)
-					abc[i]=students[i];
-				
-				for(int i=index;i<students.length-1;i++)
-					abc[i]=students[i+1];
-				students=new Student[abc.length];
-				for(int i=0;i<abc.length;i++)
-					students[i]=abc[i];
-			}
-			else
-			{
-				throw new IllegalArgumentException("Student not exist");
-			}
-		}
+		
 	}
 
 	@Override
@@ -205,51 +175,17 @@ public class StudentGroup implements StudentArrayOperation {
 
 	@Override
 	public void removeToIndex(int index) {
-		Student[] abc=new Student[index];
-		for(int i=0;i<index;i++)
-		{
-			abc[i]=students[i];
-		}
-		students=new Student[abc.length];
-		for(int i=0;i<abc.length;i++)
-			student[i]=abc[i];
+		
 	}
 
 	@Override
 	public void removeToElement(Student student) {
-		int j;
-		for(int i=0;i<students.length;i++)
-		{
-			if(students[i]==student)
-			{
-				j=i;
-				break;
-			}
-		}
-		Student[] abc=new Student[j];
-		for(int i=0;i<j;i++)
-			abc[i]=student[i];
-		Student[] students=new Student[abc.length];
-		for(int i=0;i<abc.length;i++)
-			students[i]=abc[i];
+		
 	}
 
 	@Override
 	public void bubbleSort() {
-		int c,d,n=students.length;
-		Student swap;
-		for (c = 0 ; c < ( n - 1 ); c++)
-		  {
-		    for (d = 0 ; d < n - c - 1; d++)
-		    {
-		      if (students[d] > students[d+1]) /* For decreasing order use < */
-		      {
-		        swap       = students[d];
-		        students[d]   = students[d+1];
-		        students[d+1] = swap;
-		      }
-		    }
-		  }
+		
 	}
 
 	@Override
